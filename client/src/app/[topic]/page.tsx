ba316c40ccd7f31c615feeb7523cb6c00a883778
ravikantchauhan246 <ravikantchauhan246@gmail.com>
2024-05-27 12:55:20 +0530

    voting room completed

--- client/src/app/[topic]/page.tsx
+++ client/src/app/[topic]/page.tsx
@@ -1,10 +1,27 @@
+import { redis } from "@/lib/redis"
+import ClientPage from "./ClientPage"
+
 interface PageProps{
     params:{
         topic:string
     }
 }
-const Page = ({params}:PageProps) =>{
-    return <p>{params.topic}</p>
+const Page = async ({params}:PageProps) =>{
+    const {topic}= params
+    const initialData = await redis.zrange<(string | number)[]>(`room:${topic}`,0,49,{
+        withScores:true,
+    })
+
+    const words: {text:string,value:number}[]=[]
+    for(let i=0;i<initialData.length;i++){
+        const [text,value]= initialData.slice(i,i+2)
+
+        if(typeof text === "string" && typeof value === "number"){
+            words.push({text,value})
+        }
+    }
+    await redis.incr("served_requests")
+    return <ClientPage topicName={topic} initialData={words}/>
 }
 
 export default Page
